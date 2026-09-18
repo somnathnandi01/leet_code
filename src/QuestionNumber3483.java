c17395e9c720b931b147c374690dc81b5680d684
Somnath Nandi <somnathnandi368@gmail.com>
2026-09-18 17:34:41 +0530

    optimized sol 2

--- src/QuestionNumber3483.java
+++ src/QuestionNumber3483.java
@@ -1,48 +1,53 @@
+
 import java.util.HashSet;
 import java.util.Set;
 
 public class QuestionNumber3483 {
     public int totalNumbers(int[] digits) {
         Set<Integer> set = new HashSet<>();
+        int i = 0, j = 0, k = 0;
+        int num;
 
-        for (int i = 0; i < digits.length; i++) {
-            if (digits[i] == 0) {
-                continue;
-            }
-            for (int j = 0; j < digits.length; j++) {
-                for (int k = 0; k < digits.length; k++) {
-                    if (i == j || j == k || k == i) {
-                        continue;
-                    }
-                    if (digits[k] % 2 == 0) {
-                        int number = 100 * digits[i] + 10 * digits[j] + digits[k];
-                        set.add(number);
-                        System.out.printf("%d, ", number);
-                    }
-                   
+        while (i < digits.length) {
+            if (digits[i] == 0) {i++;continue;}
+            if (i == j) {j++;continue;}
+            if (j == k) {k++; continue;}
+            if (k == i) {k++; continue;}
+
+            if (k >= digits.length) {k = 0;j++;continue;}
+            if (j >= digits.length) {j = 0;i++;continue;}
+            if (digits[k] != 0 && digits[k] % 2 != 0) {k++;continue;}
+            if (i >= digits.length) {break;}
+
+            if (i+1 < digits.length) {
+                if (digits[i] == digits[i+1]) {
+                    i++;j=0;k=0;
+                    continue;
                 }
             }
+            num = 100 * digits[i] + 10 * digits[j] + digits[k];
+            set.add(num);
+            k++;
         }
-        
-
         return set.size();
     }
 
     public static void main(String[] args) {
-        int[] digits = {1,2,3,4};
-        new QuestionNumber3483().totalNumbers(digits);
+        int[] digits = { 8,1,8 };
+        int res = new QuestionNumber3483().totalNumbers(digits);
+        System.out.println(res);
     }
 
 }
-
 
 // conditions for solving
 // 1. numbers can repeat if they have it in digits arr
 // sol -> count store digits somewhere and pull one by one to form a number
 // like stack
-// 2. 0 cannot place in first position 
+// 2. 0 cannot place in first position
 // sol -> number > 100 if not then first_digit is 0
 // 3. a number cannot repeated
 // check digits to confirm that
-// 4. need to even 
-// sol -> last digit of the number is divisable by 2 or number(or digit) % 2 == 0
+// 4. need to even
+// sol -> last digit of the number is divisable by 2 or number(or digit) % 2 ==
+// 0
